f33c63a0aff99ef8922287880fa3d8491a19efdd
yugaldekate72 <yugaldekate72@gmail.com>
2023-09-18 18:36:52 +0530

    created fetchPosts()

--- threads/lib/actions/thread.actions.ts
+++ threads/lib/actions/thread.actions.ts
@@ -1,7 +1,7 @@
 "use server"
 
-import Thread from "../models/thread.model";
 import User from "../models/user.model";
+import Thread from "../models/thread.model";
 
 import { connectToDB } from "../mongoose";
 import { revalidatePath } from "next/cache";
@@ -35,4 +35,46 @@ export async function createThread({ text, author, communityId, path }: Params){
     } catch (error: any) {
         throw new Error(`Failed to create thread: ${error.message}`);
     }
+}
+
+
+export async function fetchPosts(pageNumber = 1, pageSize = 20) {
+
+    connectToDB();
+  
+    // Calculate the number of posts to skip based on the page number and page size.
+    const skipAmount = (pageNumber - 1) * pageSize;
+  
+    // Create a query to fetch the posts that have no parent (top-level threads) (a thread that is not a comment/reply).
+    const postsQuery = Thread.find({ parentId: { $in: [null, undefined] } })
+      .sort({ createdAt: "desc" })
+      .skip(skipAmount)
+      .limit(pageSize)
+      .populate({  // populate is used to fetch data when we have reference id's to another model or current model
+        path: "author",
+        model: User,
+      })
+      .populate({
+        path: "children", // Populate the children field
+        populate: {
+          path: "author", // Populate the author field within children
+          model: User,
+          select: "_id name parentId image", // Select only _id and username fields of the author
+        },
+      });
+      
+  
+    // Count the total number of top-level posts (threads) i.e., threads that are not comments.
+    const totalPostsCount = await Thread.countDocuments({
+      parentId: { $in: [null, undefined] },
+    }); // Get the total count of posts
+  
+    const posts = await postsQuery.exec();
+
+    console.log("this is posts---------> ",posts);
+    
+  
+    const isNext = totalPostsCount > skipAmount + posts.length;
+  
+    return { posts , isNext};
 }
